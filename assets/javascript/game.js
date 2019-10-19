c03eef7ce29b5c90013c6fecfec5e4672e0fcc50
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-19 11:19:09 -0500

    Added Prettier formatting to code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ Description: In this program we will select a fighter with 3 variables attached.
 
 */
 
-
 var selectFighter = 0;
 var fight = 0;
 
@@ -11,7 +10,6 @@ var fight = 0;
 var fighter = new Object();
 //enemy object set to zero will be remaining 3 choices later
 var enemy = new Object();
-
 
 //4 RPG character choices for fight game
 var cleric = new Object();
@@ -34,96 +32,89 @@ mage.healthPoints = 140;
 mage.attackPower = 25;
 mage.counterAttackPower = 10;
 
-
 /*4 document ready functions for clicking each photo.
 If else used to pick fighter or fighting. 
 Use a function call for setFighter() to set fighter
 and a this.fight variable to set which obj we are fighting.
 */
 
-$(document).ready(function(){
-    $("#select1").on("click", function(){
+$(document).ready(function() {
+    $("#select1").on("click", function() {
         $("#select1").hide();
-        if(selectFighter == 0) {
+        if (selectFighter == 0) {
             $("#pick1").show();
             selectFighter = 1;
             setFighter(1);
-            document.getElementById("prompt").innerHTML = ("Pick who to Duel!");
-        }
-        else{
+            document.getElementById("prompt").innerHTML = "Pick who to Duel!";
+        } else {
             $("#fight1").show();
             this.fight = 1;
-            document.getElementById("prompt").innerHTML = ("Fight!");
+            document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#select2").on("click", function(){
+$(document).ready(function() {
+    $("#select2").on("click", function() {
         $("#select2").hide();
-            if(selectFighter == 0) {
+        if (selectFighter == 0) {
             $("#pick2").show();
             selectFighter = 1;
             setFighter(2);
-            document.getElementById("prompt").innerHTML = ("Pick who to Duel!");
-        }
-        else{
+            document.getElementById("prompt").innerHTML = "Pick who to Duel!";
+        } else {
             $("#fight2").show();
             this.fight = 2;
-            document.getElementById("prompt").innerHTML = ("Fight!");
+            document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#select3").on("click", function(){
+$(document).ready(function() {
+    $("#select3").on("click", function() {
         $("#select3").hide();
-        
-        if(selectFighter == 0) {
+
+        if (selectFighter == 0) {
             $("#pick3").show();
             selectFighter = 1;
             setFighter(3);
-            document.getElementById("prompt").innerHTML = ("Pick who to Duel!");
-        }
-        else{
+            document.getElementById("prompt").innerHTML = "Pick who to Duel!";
+        } else {
             $("#fight3").show();
             this.fight = 3;
-            document.getElementById("prompt").innerHTML = ("Fight!");
+            document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
-  });
+});
 
-  $(document).ready(function(){
-    $("#select4").on("click", function(){
+$(document).ready(function() {
+    $("#select4").on("click", function() {
         $("#select4").hide();
-        if(selectFighter == 0) {
+        if (selectFighter == 0) {
             $("#pick4").show();
             selectFighter = 1;
             setFighter(4);
-            document.getElementById("prompt").innerHTML = ("Pick who to Duel!");
-        }
-        else{
+            document.getElementById("prompt").innerHTML = "Pick who to Duel!";
+        } else {
             $("#fight4").show();
             this.fight = 4;
-            document.getElementById("prompt").innerHTML = ("Fight!");
+            document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
-  });
-
-
+});
 
 /*
-  setFighter takes in number 1-4 and selects which fighter you are playing as.
-  Uses a switch for the number pick.
+setFighter takes in number 1-4 and selects which fighter you are playing as.
+Uses a switch for the number pick.
 
-  Doesn't return anything but sets fighter Obj to one of our character Objs. 
+Doesn't return anything but sets fighter Obj to one of our character Objs. 
 */
-function setFighter(x){
+function setFighter(x) {
     var pick = x;
-    switch(pick){
+    switch (pick) {
         case 1:
             this.fighter = Object.assign({}, this.cleric);
-        break;
+            break;
 
         case 2:
             this.fighter = Object.assign({}, this.warrior);
@@ -137,17 +128,17 @@ function setFighter(x){
     }
 }
 /*
-  setEnemy takes in number 1-4 and selects which fighter you are battling.
-  Uses a switch for the number pick.
+setEnemy takes in number 1-4 and selects which fighter you are battling.
+Uses a switch for the number pick.
 
-  Doesn't return anything but sets enemy Obj to one of our character Objs. 
+Doesn't return anything but sets enemy Obj to one of our character Objs. 
 */
-function setEnemy(x){
+function setEnemy(x) {
     var pick = x;
-    switch(pick){
+    switch (pick) {
         case 1:
             this.enemy = Object.assign({}, this.cleric);
-        break;
+            break;
 
         case 2:
             this.enemy = Object.assign({}, this.warrior);
@@ -165,17 +156,6 @@ function setEnemy(x){
 For every attack done a different algorithm will be applied to the fighters base attack (this.attackPower).
 There is no healing. Every enemy will always use the same counter attack stat (this.counterAttackPower). 
 */
-$("attack").on("click", function(){
+$("#attack").on("click", function() {
     alert("The button was clicked.");
-  });
-
-
-
-
-
-
-
-
-
-
-
+});
