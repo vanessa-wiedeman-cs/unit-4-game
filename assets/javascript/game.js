1b90b1e6a05dc2cd70ab37278ab755d7345acb1d
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-19 18:21:44 -0500

    resized images for better fit attack button functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ Description: In this program we will select a fighter with 3 variables attached.
 */
 
 var selectFighter = 0;
-var fight = 0;
+var roundNum = 1;
 
 //fighter object set to zero will be set to 4 choices later
 var fighter = new Object();
@@ -16,21 +16,25 @@ var cleric = new Object();
 cleric.healthPoints = 100;
 cleric.attackPower = 25;
 cleric.counterAttackPower = 10;
+cleric.fight = 1;
 
 var warrior = new Object();
-warrior.healthPoints = 120;
-warrior.attackPower = 25;
-warrior.counterAttackPower = 10;
+warrior.healthPoints = 300;
+warrior.attackPower = 15;
+warrior.counterAttackPower = 20;
+warrior.fight = 2;
 
 var necromancer = new Object();
-necromancer.healthPoints = 130;
+necromancer.healthPoints = 150;
 necromancer.attackPower = 25;
 necromancer.counterAttackPower = 10;
+necromancer.fight = 3;
 
 var mage = new Object();
 mage.healthPoints = 140;
-mage.attackPower = 25;
+mage.attackPower = 30;
 mage.counterAttackPower = 10;
+mage.fight = 4;
 
 /*4 document ready functions for clicking each photo.
 If else used to pick fighter or fighting. 
@@ -48,7 +52,7 @@ $(document).ready(function() {
             document.getElementById("prompt").innerHTML = "Pick who to Duel!";
         } else {
             $("#fight1").show();
-            this.fight = 1;
+            setEnemy(1);
             document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
@@ -59,12 +63,12 @@ $(document).ready(function() {
         $("#select2").hide();
         if (selectFighter == 0) {
             $("#pick2").show();
-            selectFighter = 1;
+            selectFighter = 2;
             setFighter(2);
             document.getElementById("prompt").innerHTML = "Pick who to Duel!";
         } else {
             $("#fight2").show();
-            this.fight = 2;
+            setEnemy(2);
             document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
@@ -76,12 +80,12 @@ $(document).ready(function() {
 
         if (selectFighter == 0) {
             $("#pick3").show();
-            selectFighter = 1;
+            selectFighter = 3;
             setFighter(3);
             document.getElementById("prompt").innerHTML = "Pick who to Duel!";
         } else {
             $("#fight3").show();
-            this.fight = 3;
+            setEnemy(3);
             document.getElementById("prompt").innerHTML = "Fight!";
         }
     });
@@ -92,14 +96,22 @@ $(document).ready(function() {
         $("#select4").hide();
         if (selectFighter == 0) {
             $("#pick4").show();
-            selectFighter = 1;
+            selectFighter = 4;
             setFighter(4);
             document.getElementById("prompt").innerHTML = "Pick who to Duel!";
         } else {
             $("#fight4").show();
-            this.fight = 4;
+            setEnemy(4);
             document.getElementById("prompt").innerHTML = "Fight!";
         }
+    });
+});
+
+//document.ready function for clicking the attack button
+
+$(document).ready(function() {
+    $("#attack").on("click", function() {
+        attack();
     });
 });
 
@@ -108,6 +120,7 @@ setFighter takes in number 1-4 and selects which fighter you are playing as.
 Uses a switch for the number pick.
 
 Doesn't return anything but sets fighter Obj to one of our character Objs. 
+Also displays intial health and attack
 */
 function setFighter(x) {
     var pick = x;
@@ -126,12 +139,18 @@ function setFighter(x) {
             this.fighter = Object.assign({}, this.mage);
             break;
     }
+
+    document.getElementById("health").innerHTML =
+        "Health: " + this.fighter.healthPoints;
+    document.getElementById("power").innerHTML =
+        "Attack: " + this.fighter.attackPower;
 }
 /*
 setEnemy takes in number 1-4 and selects which fighter you are battling.
 Uses a switch for the number pick.
 
 Doesn't return anything but sets enemy Obj to one of our character Objs. 
+Also displays intial health and attack
 */
 function setEnemy(x) {
     var pick = x;
@@ -150,12 +169,85 @@ function setEnemy(x) {
             this.enemy = Object.assign({}, this.mage);
             break;
     }
+
+    document.getElementById("eHealth").innerHTML =
+        "Health: " + this.enemy.healthPoints;
+    document.getElementById("eAttack").innerHTML =
+        "Attack: " + this.enemy.attackPower;
 }
 
 /*In the fight function the fighter and enemy will battle in a series of rounds.
 For every attack done a different algorithm will be applied to the fighters base attack (this.attackPower).
 There is no healing. Every enemy will always use the same counter attack stat (this.counterAttackPower). 
 */
-$("#attack").on("click", function() {
-    alert("The button was clicked.");
-});
+function attack() {
+    if (this.enemy.fight != 0 && roundNum != 4 && this.fighter.healthPoints !=0) {
+        this.fighter.healthPoints =
+            this.fighter.healthPoints - this.enemy.counterAttackPower;
+        document.getElementById("health").innerHTML =
+            "Health: " + this.fighter.healthPoints;
+        document.getElementById("power").innerHTML =
+            "Attack: " + this.fighter.attackPower;
+
+        this.enemy.healthPoints =
+            this.enemy.healthPoints - this.fighter.attackPower;
+        document.getElementById("eHealth").innerHTML =
+            "Health: " + this.enemy.healthPoints;
+        document.getElementById("eAttack").innerHTML =
+            "Attack: " + this.enemy.attackPower;
+
+        switch (this.selectFighter) {
+            case 1:
+                this.fighter.attackPower += this.enemy.counterAttackPower;
+                this.fighter.attackPower += this.enemy.counterAttackPower
+                break;
+
+            case 2:
+                this.fighter.attackPower += this.fighter.counterAttackPower;
+                break;
+            case 3:
+                if (this.fighter.healthPoints > 75) {
+                    this.fighter.attackPower += this.fighter.counterAttackPower;
+                } else {
+                    this.fighter.attackPower += (this.fighter.counterAttackPower * 3 );
+                }
+                break;
+            case 4:
+                this.fighter.attackPower += Math.floor(Math.random() * 20) + 10;
+                break;
+        }
+
+        if (this.enemy.healthPoints <= 0) {
+            switch (this.enemy.fight) {
+                case 1:
+                    $("#fight1").hide();
+                    break;
+                case 2:
+                    $("#fight2").hide();
+                    break;
+                case 3:
+                    $("#fight3").hide();
+                    break;
+                case 4:
+                    $("#fight4").hide();
+                    break;
+            }
+            roundNum++;
+            this.enemy.fight = 0;
+            document.getElementById("round").innerHTML =
+                "Round " + roundNum + "!";
+            document.getElementById("prompt").innerHTML = "Pick who to Duel!";
+            document.getElementById("eHealth").innerHTML = "Health: ";
+            document.getElementById("eAttack").innerHTML = "Attack: ";
+        }
+
+        if (this.fighter.healthPoints <= 0) {
+            document.getElementById("prompt").innerHTML = "You died in battle!";
+        }
+    }
+
+    if (roundNum == 4) {
+        document.getElementById("prompt").innerHTML = "You win!";
+        document.getElementById("round").innerHTML = "Thanks!";
+    }
+}
